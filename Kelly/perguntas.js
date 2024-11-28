d0409ed981d2b70b3c3ae50240f4bc098b9c8fae
Get2008 <142031310+Get2008@users.noreply.github.com>
2024-11-28 11:32:16 -0300

    Add files via upload

---
+++ Kelly/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'chicago bulls',
+    'quantos titulos?',
+    '6'
+)
+
+criaCartao(
+    'Futebol',
+    'maior time do Brasil?',
+    'Corinthians'
+)
+
+criaCartao(
+    'Corinthians',
+    'Maior Freguês?',
+    'São Paulo'
+)
+
+criaCartao(
+    'GOAT',
+    'Quem é o GOAT?',
+    'Neymar'
+)
